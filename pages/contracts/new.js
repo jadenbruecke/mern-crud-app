62e591fa6b3a2099f6ad802f23d643bae51514d6
Yu Qiao <>
2023-07-14 12:37:47 +0200

    fix default value

--- pages/contracts/new.js
+++ pages/contracts/new.js
@@ -3,8 +3,8 @@ import Form from "@/components/Form"
 const NewContract = () => {
   const contractForm = {
     machineName: '',
-    oneTimeFee: '',
-    usageFee: '',
+    oneTimeFee: 0,
+    usageFee: 0,
   }
 
   return <Form formId="add-contract-form" contractForm={contractForm} />
